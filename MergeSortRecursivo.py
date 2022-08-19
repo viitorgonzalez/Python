726d109f0a36917e7ad917010f196eeb1250c7bb
viitorgonzalez <pastor123.vs@gmail.com>
2022-08-18 23:22:16 -0300

    update

---
+++ MergeSortRecursivo.py
@@ -0,0 +1,29 @@
+from heapq import merge
+
+
+def mergeSort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)
+    if (fim - inicio > 1):
+        meio = (fim + inicio) // 2
+        mergeSort(lista, inicio, meio)
+        mergeSort(lista, meio, fim)
+        merge(lista, inicio, meio. fim)
+
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    topLeft, topRight = 0, 0
+    for k in range(inicio, fim):
+        if topLeft >= len(left):
+            lista[k] = right[topRight]
+            topRight = topRight + 1
+        if topRight >= len(right):
+            lista[k] = left[topLeft]
+            topLeft  = topLeft + 1
+        if left[topLeft] < right[topRight]:
+            lista[k] = left[topLeft]
+            topLeft = topLeft + 1
+        else:
+                lista[k] = right[topRight]
+                topRight = topRight + 1
---
+++ QuickSortRecursivo.py
@@ -0,0 +1,17 @@
+def quickSort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista) - 1
+    if inicio < fim:
+        p = partition(lista, inicio, fim)
+        quickSort(lista, inicio, p - 1)
+        quickSort(lista, p + 1, fim)
+
+def partition(lista, inicio, fim):
+        pivot = lista[fim]
+        i = inicio
+        for j in range(inicio, fim):
+            if lista[j] <= pivot:
+                lista[j], lista[i] = lista[i], list[j]
+                i = i + 1
+        lista[i], lista[fim] = lista[fim], lista[i]
+        return i
